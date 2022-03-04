6953d200fb9082a5c804f14ea91554285e53432b
lialia402 <lialia402@gmail.com>
2022-03-04 16:52:36 +0200

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBO9ggIxkTIeLcfAQV7n2PtcXZPXsgWDr4",
+    authDomain: "auth-development-599b0.firebaseapp.com",
+    projectId: "auth-development-599b0",
+    storageBucket: "auth-development-599b0.appspot.com",
+    messagingSenderId: "939012328097",
+    appId: "1:939012328097:web:7243555e12fd453fd47ee0"
+  }
 };
 
 /*
